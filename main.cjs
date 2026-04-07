b1514518f195a59357c562e95f1625dbaa415e45
guoqing <240718160@qq.com>
2026-04-07 16:15:32 +0800

    up

---
+++ main.cjs
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    fullscreen: true,
+    frame: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+    return
+  }
+
+  win.loadFile(path.join(__dirname, 'dist/index.html'))
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
